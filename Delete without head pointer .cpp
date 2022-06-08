03f90ec4b2a385df35f2d71f25a39e19621f86a5
Shashwat Dev <shashwat.dev.iiit.bhubaneswar@gmail.com>
2022-06-08 21:39:13 +0530

    initial commit

---
+++ Delete without head pointer .cpp
@@ -0,0 +1,45 @@
+class Solution
+{
+    public:
+    //Function to delete a node without any reference to head pointer.
+    void deleteNode(Node *del)
+    {
+        Node *t;
+        t=del;
+        if(!t->next->next)
+        {
+            del->data=t->next->data;
+        }
+        else
+        {
+            while(t->next->next)
+            {
+               t->data=t->next->data;
+               t=t->next;
+            }
+            t->data=t->next->data;
+        }    
+        t->next=NULL;
+        /*Node *t;
+        t=head;
+        if(t==del)
+        {
+            t=head->next;
+            del->next=NULL;
+            head=t;
+        }
+        while(t)
+        {
+            if(t->next==del)
+            {
+                break;
+            }
+        }
+        if(t->next==del)
+        {
+            t->next=del->next;
+            del->next=NULL;
+        }*/
+    }
+
+};
