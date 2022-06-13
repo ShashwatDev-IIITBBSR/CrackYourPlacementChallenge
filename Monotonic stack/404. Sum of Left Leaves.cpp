4ae2ba776a9a20d1d8d701c13d1a53b362954a5f
Shashwat Dev <shashwat.dev.iiit.bhubaneswar@gmail.com>
2022-06-13 14:43:38 +0530

    initial commit

--- Monotonic stack/404. Sum of Left Leaves.cpp
+++ Monotonic stack/404. Sum of Left Leaves.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool isleaf(TreeNode* a,int &sum)
+    bool isleaf(TreeNode* a)
     {
         //if(!a){return false;}
         if((!a->left)&&(!a->right)){return true;}
@@ -8,10 +8,10 @@ public:
     }
     void LeftLeaves(TreeNode *a,int &sum)
     {
-        if(!a||isleaf(a,sum)){return;}
+        if(!a){return;}
         if(a->left!=NULL)
         {
-            if(isleaf(a->left,sum))
+            if(isleaf(a->left))
             {sum=sum+a->left->val;}
         }
         LeftLeaves(a->left,sum);
