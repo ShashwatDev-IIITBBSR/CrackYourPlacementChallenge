e6b841bd12cdae0697958949f80178f183756549
Shashwat Dev <shashwat.dev.iiit.bhubaneswar@gmail.com>
2022-06-18 23:37:46 +0530

    initial commit

--- 797. All Paths From Source to Target.cpp
+++ 797. All Paths From Source to Target.cpp
@@ -1,5 +1,34 @@
 class Solution {
 public:
+    void dfs(int val,vector<int> &dq,vector<vector<int>> &all_paths, vector<vector<int>>graph)
+    {
+        int v=graph.size()-1;
+        dq.push_back(val);
+        if(v==val)
+        {
+            all_paths.push_back(dq);
+            dq.pop_back();
+            return;
+        }
+        for(auto x:graph[val])
+        {
+            dfs(x,dq,all_paths,graph);
+        }
+        dq.pop_back();
+    }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) 
+    {
+        vector<vector<int>> all_paths;
+        vector<int> path;
+        vector<int> dq;
+        int n=graph.size();
+        vector<bool>visited(n,false);
+        dfs(0,dq,all_paths,graph);
+        return all_paths; 
+    }
+};
+/*class Solution {
+public:
     void dfs(int val,deque<int> &dq,vector<vector<int>> &all_paths, vector<vector<int>>graph)
     {
         int v=graph.size()-1;
@@ -7,7 +36,7 @@ public:
         if(v==val)
         {
             vector<int> vec;
-            while(!dq.empty())
+            while(!dq.empty())//Using Deque
             {
                 vec.push_back(dq.front());
                 dq.pop_front();
@@ -37,4 +66,4 @@ public:
         dfs(0,dq,all_paths,graph);
         return all_paths; 
     }
-};
+};*/
