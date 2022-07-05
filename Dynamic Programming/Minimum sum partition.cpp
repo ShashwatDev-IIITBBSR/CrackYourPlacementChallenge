d921fe190f4463a6e0ec0226fa1d41987aefe792
Shashwat Dev <shashwat.dev.iiit.bhubaneswar@gmail.com>
2022-07-05 15:53:43 +0530

    initial commit

---
+++ Dynamic Programming/Minimum sum partition.cpp
@@ -0,0 +1,52 @@
+class Solution{
+
+  public:
+	int minDifference(int arr[], int n)  
+	{
+	    int i,j,sum=0,w,diff;
+        for(i=0;i<n;i++)
+        {
+            sum+=arr[i];
+        }
+        w=sum/2;
+        bool mat[n+1][w+1];
+        for(i=0;i<n+1;i++)
+        {
+            for(j=0;j<w+1;j++)
+            {
+                if(j==0){mat[i][j]=1;continue;}
+                if(i==0){mat[i][j]=0;continue;}
+                if(arr[i-1]<=j)
+                {
+                    mat[i][j]=mat[i-1][j]||mat[i-1][j-arr[i-1]];
+                }
+                else
+                {
+                    mat[i][j]=mat[i-1][j];
+                }
+            }
+        }
+        if(sum%2==0)
+        {
+            diff=0;
+            i=n;j=w;
+            while((i>=0)&&(j>=0)&&(mat[i][j]==0))
+            {
+                j--;
+                diff++;
+            }
+            return diff*2;
+        }
+        else
+        {
+            diff=0;
+            i=n;j=w;
+            while((i>=0)&&(j>=0)&&(mat[i][j]==0))
+            {
+                j--;
+                diff++;
+            }
+            return diff*2+1;
+        }
+	} 
+};
