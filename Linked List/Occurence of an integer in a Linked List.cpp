2cd2956f1fae99de4392c40a0bb4d0304ee3faad
Shashwat Dev <shashwat.dev.iiit.bhubaneswar@gmail.com>
2022-09-13 11:05:37 +0530

    Create Occurence of an integer in a Linked List.cpp

---
+++ Linked List/Occurence of an integer in a Linked List.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+    public:
+    int count(struct node* head, int search_for)
+    {
+        int count=0;
+        node*temp=head;
+        while(temp!=NULL)
+        {
+            if(temp->data==search_for){count++;}
+            temp=temp->next;
+        }
+        return count;
+    }
+};
